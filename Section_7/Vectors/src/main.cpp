80188bf65a9379f930b259fddec0013af85dc1d9
Cancain <eriksson.tomas@gmail.com>
2020-01-11 19:46:23 +0100

    completed lecture 59

---
+++ Section_7/Vectors/src/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::vector;
+
+
+int main(){
+  //vector<char> vowels; //empty
+  //vecor<char> vowels(5) //length of 5 initialized to 0
+
+  vector<char> vowels {'a', 'e', 'i', 'o', 'u'};
+  cout << vowels[0] << endl;
+  cout << vowels[4] << endl;
+  //[] no bounds checking, for bounds checking use .at()
+
+  /* vector<int> test_scores(3); //length 3 uninitialized */
+  /* vector<int> test_scores(3, 100) //length 3 initialized to 100 */
+  vector<int> test_scores{100, 98, 89};
+
+  cout << "\nTest scores using array syntax" << endl;
+  cout << test_scores[0] << endl;
+  cout << test_scores[1] << endl;
+  cout << test_scores[2] << endl;
+
+  cout << "\nTest scores using vector syntax" << endl;
+  cout << test_scores.at(0) << endl;
+  cout << test_scores.at(1) << endl;
+  cout << test_scores.at(2) << endl;
+
+  cout << "\nThere are " << test_scores.size() << " scores in the vector" << endl;
+
+  cout << "enter three test scores: " << endl;
+  cin >> test_scores.at(0);
+  cin >> test_scores.at(1);
+  cin >> test_scores.at(2);
+
+  cout << "\nUpdated test scores: " << endl;
+  cout << test_scores.at(0) << endl;
+  cout << test_scores.at(1) << endl;
+  cout << test_scores.at(2) << endl;
+
+  cout << "\nEnter a test score to add to the vector: " << endl;
+
+  int score_to_add {0};
+  cin >> score_to_add;
+  test_scores.push_back(score_to_add);
+
+  cout << "\nEnter an other test score to add to the vector: " << endl;
+  cin >> score_to_add;
+  test_scores.push_back(score_to_add);
+
+  cout << "\nTest scores are now: " << endl;
+  cout << test_scores.at(0) << endl;
+  cout << test_scores.at(1) << endl;
+  cout << test_scores.at(2) << endl;
+  cout << test_scores.at(3) << endl;
+  cout << test_scores.at(4) << endl;
+
+  cout << "\nThere are now" << test_scores.size() << " scores in the vector" << endl;
+
+  /* cout << "This should cause an exception " << test_scores.at(10) << endl; */
+
+  //Example of an 2d vector
+  vector<vector<int>> movie_ratings {
+    {1,2,3,4,5},
+    {1,2,3,4,5},
+    {1,2,3,4,5}
+  };
+
+  cout << "\nHere are the movie ratings for reviewer #1 using array syntax :" << endl;
+  cout << movie_ratings[0][0] << endl;
+  cout << movie_ratings[0][1] << endl;
+  cout << movie_ratings[0][2] << endl;
+  cout << movie_ratings[0][3] << endl;
+
+  cout << "\nHere are the movie ratings for reviewer #1 using vector syntax :" << endl;
+  cout << movie_ratings.at(0).at(0) << endl;
+  cout << movie_ratings.at(0).at(1) << endl;
+  cout << movie_ratings.at(0).at(2) << endl;
+  cout << movie_ratings.at(0).at(3) << endl;
+
+  return 0; 
+}
