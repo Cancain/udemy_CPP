3e4fffa3089c586e33f091c59af647e7d079df92
Cancain <eriksson.tomas@gmail.com>
2020-04-27 19:40:18 +0200

    completed lecture 102 and exercise1

---
+++ Section_11/user_defined_functions/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int add_numbers(int a, int b){
+  if(a < 0 || b < 0)
+    return 0;
+  else 
+    return a + b;
+}
+
+int main(){
+  cout << add_numbers(1, 2) << endl;
+}
