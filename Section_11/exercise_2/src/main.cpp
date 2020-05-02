1e8ef8361ffb76b8528ae62426242c960fa63f1e
Cancain <eriksson.tomas@gmail.com>
2020-05-02 13:29:46 +0200

    completed section11 excercise 2

---
+++ Section_11/exercise_2/src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+//----WRITE YOUR FUNCTION PROTOTYPES BELOW THIS LINE----
+
+double fahrenheit_to_celsius(double fahrenheit);
+double fahrenheit_to_kelvin(double fahrenheit);
+
+//----WRITE YOUR FUNCTION PROTOTYPES ABOVE THIS LINE----
+//----DO NOT MODIFY THE CODE BELOW THIS LINE----
+
+void temperature_conversion(double fahrenheit_temperature) {
+    
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //----WRITE YOUR FUNCTION CALLS BELOW THIS LINE----
+
+    double celsius_temperature {fahrenheit_to_celsius(fahrenheit_temperature)};
+    double kelvin_temperature {fahrenheit_to_kelvin(fahrenheit_temperature)};
+    
+    //----WRITE YOUR FUNCTION CALLS ABOVE THIS LINE----
+    //----DO NOT MODIFY THE CODE BELOW THIS LINE----
+    
+    cout << "The fahrenheit temperature " << fahrenheit_temperature << " degrees is equivalent to " << celsius_temperature << " degrees celsius and " << kelvin_temperature << " degrees kelvin.";
+}
+
+//----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+//----WRITE YOUR FUNCTION DEFINITIONS BELOW THIS LINE----
+
+double fahrenheit_to_celsius(double fahrenheit){
+  double result = (5.0 / 9.0) * (fahrenheit - 32);
+  return round(result);
+}
+
+double fahrenheit_to_kelvin(double fahrenheit){
+  double result = (5.0 / 9.0) * (fahrenheit - 32) + 273;
+  return round(result);
+}
+
+
+
